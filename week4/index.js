e6419f7010a2c6ebe27204dff6ab3de1132bc055
Back-end Class A <back-endclassa@DSIs-iMac.lan>
2021-06-21 12:00:28 +0100

    ICTYEP

--- week4/index.js
+++ week4/index.js
@@ -1,4 +1,12 @@
-var greeting = function (person) {
-    console.log('good day' + person);
-};
-greeting('Daniel');
+// const greeting = (person: string) => {
+//     console.log('good day' + person);
+// };
+// greeting('Daniel')
+var Useraccount = /** @class */ (function () {
+    function Useraccount(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+    return Useraccount;
+}());
+var user = new Useraccount("Murphy", 1);
--- week4/index.ts
+++ week4/index.ts
@@ -1,4 +1,22 @@
-const greeting = (person: string) => {
-    console.log('good day' + person);
-};
-greeting('Daniel')
+// const greeting = (person: string) => {
+//     console.log('good day' + person);
+// };
+// greeting('Daniel')
+
+interface User {
+    name: string;
+    id: number;
+}
+
+class Useraccount {
+    name: string;
+    id: number
+
+
+constructor(name: string, id: number){
+    this.name= name;
+    this.id = id
+}
+}
+
+const user: User = new Useraccount ("Murphy" , 1);
