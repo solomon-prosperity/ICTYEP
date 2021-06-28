9c72bf8e36aa7fcb3f9ca17c15c95640e71ec2b8
Back-end Class A <back-endclassa@DSIs-iMac.lan>
2021-06-28 12:02:01 +0100

    week5

---
+++ week5/index.js
@@ -0,0 +1,8 @@
+// let a = 11;
+
+// (a === 5 ) ? console.log(`true`) : (a === 10) ? console.log(`false`) 
+// : console.log ("NAN");
+
+window.addEventListener('click' , () => {
+    alert(`page fully loaded, enjoy!`)
+}) 
